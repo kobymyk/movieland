dc103be982ed2abe57d40dd1442037d27cb2a79e
kobymyk <kobylkin@gmail.com>
2018-11-22 09:53:15 +0200

    UTF8

--- src/main/java/db2/onlineshop/web/controller/MovieController.java
+++ src/main/java/db2/onlineshop/web/controller/MovieController.java
@@ -11,11 +11,13 @@ import db2.onlineshop.web.transform.impl.ParamConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Map;
 
@@ -43,7 +45,7 @@ public class MovieController {
         return result;
     }
 
-    @RequestMapping(path = "/v1/movie", method = RequestMethod.GET)
+    @RequestMapping(path = "/v1/movie", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public String getJsonMovies() throws JsonProcessingException {
         log.info("getMoviesAsJson/start");
