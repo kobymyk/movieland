c14ddfac63c6f3666f5223c1d816d20b40d0dfeb
kobymyk <kobylkin@gmail.com>
2018-11-23 10:10:33 +0200

    MovieJsonController returns List<Movie>

--- src/main/java/db2/onlineshop/web/controller/MovieJsonController.java
+++ src/main/java/db2/onlineshop/web/controller/MovieJsonController.java
@@ -9,16 +9,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "/v1/movie")
 public class MovieJsonController {
-    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();;
     private final Logger log = LoggerFactory.getLogger(getClass());
     @Value("${movie.randomCount:3}")
     private int randomCount;
@@ -27,24 +26,20 @@ public class MovieJsonController {
     private MovieService movieService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseBody
-    public String getAllMovies() throws JsonProcessingException {
+    public List<Movie> getAllMovies() {
         log.info("getMoviesAsJson/start");
         long startTime = System.currentTimeMillis();
-        List<Movie> items = movieService.getMovies();
-        String result = JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(items);
+        List<Movie> result = movieService.getMovies();
         log.info("getMoviesAsJson:duration={}", System.currentTimeMillis() - startTime);
 
         return result;
     }
 
     @GetMapping(path = "/random", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseBody
-    public String getRandomMovies() throws JsonProcessingException {
+    public List<Movie> getRandomMovies() {
         log.info("getJsonRandomMovies/start");
         long startTime = System.currentTimeMillis();
-        List<Movie> items = movieService.getRandomMovies(randomCount);
-        String result = JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(items);
+        List<Movie> result = movieService.getRandomMovies(randomCount);
         log.info("getJsonRandomMovies:duration={}", System.currentTimeMillis() - startTime);
 
         return result;
