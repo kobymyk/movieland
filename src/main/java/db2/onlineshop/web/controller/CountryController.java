a5c88d54a42d55865559f56a25aecba9bc2a60f2
kobymyk <kobylkin@gmail.com>
2019-01-25 21:12:08 +0200

    config

--- src/main/java/db2/onlineshop/web/controller/CountryController.java
+++ src/main/java/db2/onlineshop/web/controller/CountryController.java
@@ -6,20 +6,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(value = "/v1/country")
+@Controller
 public class CountryController {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private CountryService countryService;
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @ResponseBody
+    @GetMapping(value = "/v1/country", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public List<Country> getAll() {
         log.info("getAll");
         long startTime = System.currentTimeMillis();
@@ -27,6 +30,14 @@ public class CountryController {
         log.info("getAll:duration={}", System.currentTimeMillis() - startTime);
 
         return result;
+    }
+
+    @GetMapping(value = "/country")
+    public String list(Model model) {
+        List<Country> countries = countryService.getAll();
+        model.addAttribute("countries", countries);
+
+        return "countryList.html";
     }
 
     @Autowired
