f8be1f67254b7efd30065ec21444cbbd9f9a8db5
dmitry_burlak <mailtoxtr@gmail.com>
2021-10-31 21:32:17 +0200

    securityfilter is working

--- src/main/java/db2/onlineshop/config/AppInitializer.java
+++ src/main/java/db2/onlineshop/config/AppInitializer.java
@@ -1,16 +1,18 @@
 package db2.onlineshop.config;
 
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class AppInitializer implements WebApplicationInitializer {
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     public void onStartup(final ServletContext context) throws ServletException {
         AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
@@ -23,5 +25,20 @@ public class AppInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic appServlet = context.addServlet("app", dispatcherServlet);
         appServlet.setLoadOnStartup(1);
         appServlet.addMapping("/");
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {SecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
     }
 }
---
+++ src/main/java/db2/onlineshop/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package db2.onlineshop.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
